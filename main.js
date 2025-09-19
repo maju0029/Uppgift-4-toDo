451bc841768bf69c877fe4118a7ba1cfebe12997
maju0029 <maju0029@gmail.com>
2025-09-19 10:32:56 +0200

    första commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//javascript för todolista
+
+const myPtag = document.querySelectorAll("p");
+const usertext = document.querySelector("#usertext");
+const colorButton = document.querySelector("#id");
