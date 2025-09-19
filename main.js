43b77af10fda21caa5daf8fe066c874293d3e8f9
maju0029 <maju0029@gmail.com>
2025-09-19 17:40:54 +0200

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-//javascript för todolista
+//javascript för todolista. För move item. "Bra att ha samma id som på variabeln".
+const completedElement = document.querySelector("#completedElement");
+const inputToDo = document.querySelector("#inputToDo");
+const addTodoBtn = document.querySelector("#addTodoBtn");
+const infoTextElement = document.querySelector("small");
 
-const myPtag = document.querySelectorAll("p");
-const usertext = document.querySelector("#usertext");
-const colorButton = document.querySelector("#id");
+const TodoList = document.querySelector("#todoList");
+const secondList = document.querySelector("#secondList");
+addTodoBtn.addEventListener("click", addTodo);
+
+function addTodo() {
+    //Kod för att flytta ett objekt från input till en lista.
+    
+    const firstElement = firstList.firstElementChild
+    secondList.appendChild(firstElement);
+}
