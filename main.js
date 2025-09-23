7776a1a6d38e5236d1bdd763133262e6f20adb98
maju0029 <maju0029@gmail.com>
2025-09-23 12:41:15 +0200

    Fixing trashCan

--- main.js
+++ main.js
@@ -11,21 +11,21 @@ const secondList = document.querySelector("#secondList");
 addTodoBtn.addEventListener("click", addTodo);
 
 function changeStatus(todoText, completed) {
-    
+
     let findIndex = allTheTodos.map(t => t.name).indexOf(todoText);
     allTheTodos[findIndex].completed = completed;
 }
 
 function addTodo() {
-    
-    infoTextElement.textContent ="";
+
+    infoTextElement.textContent = "";
     todoText = inputToDo.value;
     if (todoText === "") {
         infoTextElement.textContent = "Men snälla du, skriv in något!";
         return;
     }
 
-    
+
     const todoObject = { name: todoText, completed: false };
     allTheTodos.push(todoObject);
 
@@ -35,33 +35,44 @@ function addTodo() {
     const itemText = document.createElement('span');
 
     itemText.innerText = todoText;
-        itemText.addEventListener("click", 
-            function() {
-                if (itemText.classList.contains("completed")) {
-                    itemText.setAttribute("class", "");
-                    completed--;
-                    completedElement.textContent = "Du har " + completed + " slutförda uppgifter";
-                    changeStatus(itemText.innerText, false);
-                } else {
-                    itemText.setAttribute("class", "completed");
-                    completed++;
-                    completedElement.textContent = "Du har " + completed + " slutförda uppgifter";
-                    changeStatus(itemText.innerText, true);
-                }
+    itemText.addEventListener("click",
+        function () {
+            if (itemText.classList.contains("completed")) {
+                itemText.setAttribute("class", "");
+                completed--;
+                completedElement.textContent = "Du har " + completed + " slutförda uppgifter";
+                changeStatus(itemText.innerText, false);
+            } else {
+                itemText.setAttribute("class", "completed");
+                completed++;
+                completedElement.textContent = "Du har " + completed + " slutförda uppgifter";
+                changeStatus(itemText.innerText, true);
             }
-        );
-        item.appendChild(itemText);
+        }
+    );
+    item.appendChild(itemText);
 
     const trashcan = document.createElement('span');
     trashcan.innerHTML = '&#128465';
     trashcan.classList.add("trashcan");
     item.appendChild(trashcan);
-    trashcan.addEventListener("click", function() {
+    trashcan.addEventListener("click", function () {
         item.remove();
         let removeText = itemText.firstChild.textContent;
         let indexToRemove = allTheTodos.map(t => t.name).indexOf(removeText);
+
+        if (itemText.classList.contains("completed")) {
+            completed--;
+        }
+
         allTheTodos.splice(indexToRemove, 1);
+
+        if (allTheTodos.length === 0) {
+            completedElement.textContent = "";
+        } else {
+            completedElement.textContent = "Du har " + completed + " slutförda uppgifter";
+        }
     })
 
-        inputToDo.value = "";
-    }
+    inputToDo.value = "";
+}
